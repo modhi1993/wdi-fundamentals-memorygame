309082a2a0dc1aae8d50abe2e77909df9842e84b
Modhi Aldughaiyem <modhialdughaiyem@Modhis-MacBook-Pro.local>
2019-11-08 15:20:00 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,5 +3,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+alert('Hello, friends.');
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log(cardOne)
